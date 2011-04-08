bc6fd2caed785b8d1d59e02bdac5ef20ca3d30ef
kirillzonov <kirillzonov@flatsoft.com>
2011-04-08 17:52:21 +0400

    dependent delete_all

--- app/models/dvd.rb
+++ app/models/dvd.rb
@@ -2,7 +2,7 @@ class Dvd < ActiveRecord::Base
   validates :name, :user, :presence => true
   
   belongs_to :user
-  has_many :reels
+  has_many :reels, :dependent => :delete_all
 
   def to_s
     name
