353101d4626f97ca9446c86a1ee7d4e6a504e94a
kirillzonov <kirillzonov@flatsoft.com>
2011-04-11 16:00:31 +0400

    smtp instead sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DVD::Application.initialize!
+DVD::Application.configure do
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => 'rails.fs.test@gmail.com',
+    :password       => 'rails3base'
+  }
+end
+
